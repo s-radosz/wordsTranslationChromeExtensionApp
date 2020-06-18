8d31e8a1b9c70ef706886bfce2de7216344ddad0
s-radosz <radoszszymon@gmail.com>
2020-06-18 19:27:05 +0200

    updated composer.json

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@ php artisan cache:clear
 php artisan config:clear
 rm -rf ~/domains/praktyczny-angielski.pl/public_html/js/*
 rm -rf ~/domains/praktyczny-angielski.pl/public_html/css/*
-mv ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/js/app.js ~/domains/praktyczny-angielski.pl/public_html/js/
-mv ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/js/app.css ~/domains/praktyczny-angielski.pl/public_html/css/
+mv ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/js/* ~/domains/praktyczny-angielski.pl/public_html/js/.
+mv ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/js/* ~/domains/praktyczny-angielski.pl/public_html/css/.
 
 
