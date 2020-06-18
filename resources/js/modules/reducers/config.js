1309c4fdc91fc3cdb20808fdb9a66eadc798992d
s-radosz <radoszszymon@gmail.com>
2020-06-18 19:51:30 +0200

    fixed composerExhancer

--- resources/js/modules/reducers/config.js
+++ resources/js/modules/reducers/config.js
@@ -2,10 +2,10 @@ import actionTypes from "./../actionTypes"
 
 const defaultState = {
     paths: {
-        // APP_URL: "http://127.0.0.1:8000",
-        // API_URL: "http://127.0.0.1:8000/api"
-        APP_URL: "http://www.praktyczny-angielski.pl",
-        API_URL: "http://www.praktyczny-angielski.pl/api"
+        APP_URL: "http://127.0.0.1:8000",
+        API_URL: "http://127.0.0.1:8000/api"
+        // APP_URL: "http://www.praktyczny-angielski.pl",
+        // API_URL: "http://www.praktyczny-angielski.pl/api"
     },
     showLoader: false,
     alert: {
--- resources/js/modules/store.js
+++ resources/js/modules/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import reducer from "./reducer";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
 
 export default function configureStore(initialState) {
     const store = createStore(
