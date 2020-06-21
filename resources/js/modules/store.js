cdc22dee4474b79cac775fe2bf3554e873e5f034
s-radosz <radoszszymon@gmail.com>
2020-06-21 17:46:19 +0200

    disable redux

--- resources/js/modules/store.js
+++ resources/js/modules/store.js
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import reducer from "./reducer";
 
-const composeEnhancer = typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+// const composeEnhancer = typeof window === 'object' &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const composeEnhancer = compose;
 
 export default function configureStore(initialState) {
     const store = createStore(
