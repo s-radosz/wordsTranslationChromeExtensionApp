1bad0862000f4c597898cc50ae54fb2a9b8e6ecd
s-radosz <radoszszymon@gmail.com>
2020-08-19 21:06:12 +0200

    prod paths

--- resources/js/modules/reducers/config.js
+++ resources/js/modules/reducers/config.js
@@ -4,10 +4,10 @@ const defaultState = {
     paths: {
         APP_URL: process.env.MIX_APP_URL
             ? process.env.MIX_APP_URL
-            : "http://www.praktyczny-angielski.pl",
+            : "http://praktyczny-angielski.pl",
         API_URL: process.env.MIX_APP_URL
             ? `${process.env.MIX_APP_URL}/api`
-            : "http://www.praktyczny-angielski.pl/api"
+            : "http://praktyczny-angielski.pl/api"
         // APP_URL: "https://www.praktyczny-angielski.pl",
         // API_URL: "https://www.praktyczny-angielski.pl/api"
     },
