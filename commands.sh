1dfca6df1864bb3ea09375821a4cbffa8e758f58
s-radosz <radoszszymon@gmail.com>
2020-08-19 21:40:16 +0200

    sh file for configure on shared hosting

--- commands.sh
+++ commands.sh
@@ -3,9 +3,7 @@ npm i
 php artisan jwt:secret
 php artisan cache:clear
 php artisan config:clear
-rm -rf ~/domains/praktyczny-angielski.pl/public_html/js/*
-rm -rf ~/domains/praktyczny-angielski.pl/public_html/css/*
-mv ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/js/* ~/domains/praktyczny-angielski.pl/public_html/js/.
-mv ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/css/* ~/domains/praktyczny-angielski.pl/public_html/css/.
+cp -a ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/js/app.js ~/domains/praktyczny-angielski.pl/public_html/js/
+cp -a ~/domains/praktyczny-angielski.pl/wordsTranslationChromeExtensionApp/public/css/app.css ~/domains/praktyczny-angielski.pl/public_html/css/
 composer update
 
